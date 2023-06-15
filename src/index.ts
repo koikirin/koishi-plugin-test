39bc1ed4b18e0e3bfb13545d971485e73905d2cd
Hieuzest <girkirin@hotmail.com>
2023-06-16 07:02:56 +0800

    feat: add self message redirect

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,19 @@ export class TestService extends Service {
     super(ctx, 'test', true)
     ctx.plugin(Foo)
     
+    // Remove low-authority warning
     ctx.before('send', (session) => {
       if (session.content === '权限不足。') return true
+    })
+
+    // Handle self message
+    ctx.guild().on('message', (session) => {
+      if (session.userId === session.bot.selfId && session.content.slice(0, 2) === '//') {
+        let newSession = session.bot.session(session);
+        newSession.userId = '@self'
+        newSession.content = newSession.content.slice(2)
+        session.bot.dispatch(newSession)
+      }
     })
   }
 }
