15d19371612d311b64d1e0caef114d9df64dade5
Hieuzest <girkirin@hotmail.com>
2023-06-16 18:45:35 +0800

    feat: add selfSendPrefix config and support auto remove before-send

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Service } from 'koishi'
+import { Context, Schema, Service } from 'koishi'
 import { Foo } from './foo'
 
 
@@ -11,8 +11,11 @@ declare module 'koishi' {
 export class TestService extends Service {
   a: string
 
-  constructor(ctx: Context) {
+  constructor(ctx: Context, private config: TestService.Config) {
     super(ctx, 'test', true)
+
+    const selfSendPrefixLength = config.selfSendPrefix.length
+
     ctx.plugin(Foo)
     
     // Remove low-authority warning
@@ -22,14 +25,35 @@ export class TestService extends Service {
 
     // Handle self message
     ctx.guild().on('message', (session) => {
-      if (session.userId === session.bot.selfId && session.content.slice(0, 2) === '//') {
+      if (session.userId === session.bot.selfId &&
+        session.content.slice(0, selfSendPrefixLength) === config.selfSendPrefix) {
         let newSession = session.bot.session(session);
         newSession.userId = '@self'
-        newSession.content = newSession.content.slice(2)
+        newSession.content = newSession.content.slice(selfSendPrefixLength)
         session.bot.dispatch(newSession)
       }
     })
+
+    ctx.before('send', (session) => {
+      if (session.content.slice(0, selfSendPrefixLength) === config.selfSendPrefix) {
+        let newSession = session.bot.session(session);
+        newSession.userId = '@self'
+        newSession.content = newSession.content.slice(selfSendPrefixLength)
+        session.bot.dispatch(newSession)
+      }
+    })
+
   }
+}
+
+export namespace TestService {
+  export interface Config {
+    selfSendPrefix: string
+  }
+
+  export const config: Schema<Config> = Schema.object({
+    selfSendPrefix: Schema.string().default('//')
+  })
 }
 
 export default TestService
