54d38d99a3760d96b6bca94d62b4ac17e9879939
Hieuzest <girkirin@hotmail.com>
2023-07-09 20:47:50 +0800

    feat: info sessions, forward settings

--- src/foo.ts
+++ src/foo.ts
@@ -1,8 +1,70 @@
-import { Context } from 'koishi'
+import { Context, Service } from 'koishi'
 
-export class Foo {
-  constructor(ctx: Context) {
+
+declare module 'koishi' {
+  interface Tables {
+    'foo/test': Foo.TestFields
   }
 }
+
+export class Foo extends Service {
+
+  constructor(ctx: Context) {
+    super(ctx, 'foo', true)
+    // ctx.using(['database'], async () => {
+    //   ctx.model.drop('foo/test')
+    console.log('Foo service initializing.')
+
+    ctx.model.extend('foo/test', {
+      id: 'unsigned',
+      a: 'unsigned',
+      b: 'boolean',
+      c: 'boolean'
+    }, { primary: ['id', 'a'] })
+ 
+    //   await ctx.database.create('foo/test', { a: 1, b: true, c: true })
+    //   await ctx.database.create('foo/test', { a: 1, b: false, c: true })
+    //   await ctx.database.create('foo/test', { a: 1, b: false, c: false })
+  
+    //   let res = await ctx.database.get('foo/test', {
+    //     b: false,
+    //     c: true
+    //   })
+    //   console.log('Result', res)
+    // })
+    // console.log(ctx.database.tables.user.fields)
+
+    ctx.command('foo.testdb <cnt:number>').action(async (argv, cnt) => {
+      // console.log(argv)
+      // config.testConfig = 'a'
+      // TestService.Config = Schema.object({
+      //   selfSendPrefix: Schema.string().default('//'),
+      //   testConfig: Schema.union(['c', 'd'])
+      // })
+      console.log(typeof cnt, cnt)
+      const records: Partial<Foo.TestFields>[] = []
+      for (const i of [...Array(cnt).keys()]) records.push({
+        id: 10000 + i,
+        a: i,
+        b: true,
+        c: false
+      })
+      // console.log(records)
+      await ctx.database.upsert('foo/test', records)
+      return 'Hello!'
+    })
+
+  }
+}
+
+export namespace Foo {
+  export interface TestFields {
+    id: number
+    a: number
+    b: boolean
+    c: boolean
+  }
+}
+
 
 export default Foo
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,63 @@
-import { Context, Schema, Service } from 'koishi'
-import { Foo } from './foo'
-
+import { } from '@koishijs/plugin-adapter-onebot'
+import { } from '@koishijs/plugin-adapter-kook'
+import { } from '@koishijs/plugin-auth'
+import { } from '@koishijs/plugin-console'
+import { Context, Element, Loader, Logger, Schema, Service, Session, h } from 'koishi'
 
 declare module 'koishi' {
   interface Context {
     test: TestService
+    // foo: Foo
   }
 }
 
+const logger = new Logger('test')
+
 export class TestService extends Service {
-  a: string
+  static using = ['database']
+  
+  _findPlugin(name: string, parent: Context): [string, Context, Context] {
+    if (!parent) return
+    const reg = parent.scope[Loader.kRecord]
+    if (!reg) return
+    for (const key of Object.getOwnPropertyNames(reg)) {
+      const i1 = key.indexOf('/'), i2 = key.indexOf(':')
+      const mkey = key.slice(i1 === -1 ? 0 : i1+1, i2 === i1 ? key.length: i2)
+      // console.log(mkey)
+      // if (mkey === 'group') continue
+      if (mkey === name) return [key, parent, reg[key]?.ctx]
+      // console.log(reg[key]?.ctx)
+      const res = this._findPlugin(name, reg[key]?.ctx)
+      if (res) return res
+    }
+  }
+
+  findPlugin(name: string) {
+    return this._findPlugin(name, this.ctx.loader.entry)
+  }
 
   constructor(ctx: Context, private config: TestService.Config) {
     super(ctx, 'test', true)
 
-    const selfSendPrefixLength = config.selfSendPrefix.length
+    const selfSendPrefixLength = config.selfSendPrefix?.length
 
     console.log('Test plugin initializing.')
 
-    ctx.plugin(Foo)
-    
+    // ctx.plugin(Foo)
+
+
     ctx.middleware((session, next) => {
       if (session.content === 'help') return
+      // console.log(session)
       return next()
     }, true)
 
     // Handle self message
     ctx.guild().on('message', (session) => {
-      if (session.userId === session.bot.selfId &&
+      if (config.infoAllSessions === 'event') {
+        logger.info(session)
+      }
+      if (session.userId === session.bot.selfId && selfSendPrefixLength &&
         session.content.slice(0, selfSendPrefixLength) === config.selfSendPrefix) {
         let newSession = session.bot.session(session);
         newSession.userId = '@self'
@@ -37,7 +67,7 @@ export class TestService extends Service {
     })
 
     ctx.before('send', (session) => {
-      if (session.content.slice(0, selfSendPrefixLength) === config.selfSendPrefix) {
+      if (selfSendPrefixLength && session.content.slice(0, selfSendPrefixLength) === config.selfSendPrefix) {
         let newSession = session.bot.session(session);
         newSession.userId = '@self'
         newSession.content = newSession.content.slice(selfSendPrefixLength)
@@ -45,16 +75,97 @@ export class TestService extends Service {
       }
     })
 
+    const forwardToMe = (session: Session, msg: Element.Fragment) => {
+      session.bot.sendPrivateMessage(config.forwardUserId || session.selfId, msg)
+    }
+
+    // Handle tome message
+    ctx.private().middleware((session, next) => {
+      if (session.userId == session.selfId) return next()
+      return next((next) => {
+        forwardToMe(session, h('', `From ${session.author.username}(${session.author.userId})\n`, session.elements))
+      })
+    })
+
+    ctx.guild().middleware((session, next) => {
+      let flag = false
+      if (session.quote && session.quote.userId === session.selfId) flag = true
+      else for (const ele of session.elements) {
+        if (ele.type == 'at' && ele.attrs.id === session.selfId) {
+          flag = true
+          break
+        }
+      }
+      if (flag) return next((next) => {
+        forwardToMe(session, h('', `From ${session.author.nickname}(${session.author.userId}) from ${session.guildName}(${session.guildId}):`, session.elements))
+      })
+      else return next()
+    })
+
+    if (config.infoAllSessions === 'middleware') {
+      // ctx.on('guild-member/ban', (session) => {
+      //   ctx.logger('test').info(session.onebot?.sub_type)
+      // })
+      ctx.middleware((session, next) => {
+        if (config.testMode === 'all' || !session.userId)
+          logger.info(session)
+        return next()
+      }, true)
+    }
+
+
+    if (config.fixChannelName) {
+      ctx.guild().middleware(async (session, nect) => {
+        const channel = await session.getChannel()
+        // if (session.channelName && channel.)
+        
+      })
+    }
+
+    ctx.command('test.real').action((argv) => {
+      const [key, parent, plugin] = this.findPlugin('adapter')??[]
+      console.log(plugin.scope.runtime.ctx['__CHRONO_LAUNCHER__'])
+      return 'Hello!'
+    })
+
+
+    ctx.command('test.rel').userFields(['locales']).action(async (argv) => {
+      console.log(argv.session.text('general.name'), argv.session.user.locales)
+      return await argv.session.execute('test.real')
+      return '.......'
+    })
+
+    ctx.command('test.reload <plugin:string>').action(async (argv, name) => {
+      const [key, parent, plugin] = this.findPlugin(name)??[]
+      if (!key) return 'Not found'
+      ctx.loader.unloadPlugin(parent, key)
+      await ctx.loader.reloadPlugin(parent, key, parent.config)
+      
+      return 'Success' + plugin
+    })
+
+    // ctx.on('send', (session) => {
+    //   // console.log('before-execute', argv)
+    //   console.log(session)
+    // })
   }
 }
 
 export namespace TestService {
   export interface Config {
-    selfSendPrefix: string
+    forwardUserId?: string
+    selfSendPrefix?: string
+    infoAllSessions: 'off' | 'middleware' | 'event'
+    testMode: 'all' | 'undefined-userid'
+    fixChannelName: boolean
   }
 
-  export const Config: Schema<Config> = Schema.object({
-    selfSendPrefix: Schema.string().default('//')
+  export let Config: Schema<Config> = Schema.object({
+    forwardUserId: Schema.string(),
+    selfSendPrefix: Schema.string().default('//'),
+    infoAllSessions: Schema.union(['off', 'middleware', 'event'] as const).default('off'),
+    testMode: Schema.union(['all', 'undefined-userid'] as const).default('all'),
+    fixChannelName: Schema.boolean().default(false),
   })
 }
 
