9afd8bb80c907521017cd9cedc46f47334361d97
Hieuzest <girkirin@hotmail.com>
2023-09-03 13:01:17 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import { } from '@hieuzest/koishi-plugin-send'
 import { } from '@koishijs/plugin-market'
-import { Context, Element, h, Loader, Logger, noop, Schema, Service, Session } from 'koishi'
+import { Argv, Context, Element, h, Loader, Logger, noop, Schema, Service, Session } from 'koishi'
 import { } from '@hieuzest/koishi-plugin-adapter-red'
+import { } from '@hieuzest/koishi-plugin-permissions'
+// import { resolve } from 'path'
 
 declare module 'koishi' {
   interface Context {
     test: TestService
     // foo: Foo
   }
+
+  interface Tables {
+    'test/f': TestF
+  }
+}
+
+interface TestF {
+  name: string
+  b: boolean
 }
 
 const logger = new Logger('test')
@@ -50,6 +61,41 @@ export class TestService extends Service {
 
     // ctx.plugin(Foo)
     console.log('Test plugin initializing.')
+    ctx.model.extend('test/f', {
+      name: 'string',
+      b: 'boolean',
+    }, {
+      // autoInc: true,
+      primary: ['name'],
+    })
+
+    // ctx.database.create('test/f', { name: '1', b: true })
+    // ctx.database.create('test/f', { name: '2', b: false })
+
+    // ctx.before('send', async (session, options) => {
+    //   console.log(session.content)
+    //   return true
+    // })
+
+    ctx.on('guild-member-added', session => {
+      console.log(session)
+    })
+
+    ctx.guild().middleware((session, next) => {
+      console.log(session.elements)
+      return next()
+    })
+
+    ctx.command('hrecall <msgId:string>').action(({ session }, msgId) => {
+      session.red.recall([msgId], 2, session.channelId)
+    })
+
+    ctx.guild().middleware(async (session, next) => {
+      // if (!session.red.roleType) {
+      logger.info('[test role] %s:%s %s', session.channelId, session.userId, session.red?.roleType, session.author)
+      // }
+      return next()
+    }, true)
 
     ctx.middleware(async (session, next) => {
       if (session.content.trim().toLowerCase() === 'help') return
@@ -137,9 +183,11 @@ export class TestService extends Service {
       }, true)
     }
 
-    ctx.command('test', { authority: 5 }).action(noop)
+    ctx.permissions.inherit('custom.test.admin', 'custom.test.operator')
 
-    ctx.command('test.image')
+    ctx.command('test', { permissions: [] }).action(noop)
+
+    ctx.command('test.image', { permissions: ['custom.test.operator'] })
       .option('url', '-u <url:string>', { fallback: 'https://koishi.chat/logo.png' })
       .option('mime', '-m <mime:string>', { fallback: 'image/png' })
       .action(async ({ session, options, args }) => {
@@ -148,18 +196,18 @@ export class TestService extends Service {
           .then(resp => Buffer.from(resp.data, 'binary')).then(b => h.image(b, options.mime))
       })
 
-    ctx.command('test.real <arg:number>', { checkUnknown: true, checkArgCount: true })
+    ctx.command('test.real <arg:number>', { checkUnknown: true, checkArgCount: true, permissions: ['custom.test.operator'] })
       .option('-w', 'www')
       .action(({ session, options, args }) => {
         return JSON.stringify({ options, args })
       })
 
-    ctx.command('test.rel').userFields(['locales']).action(async (argv) => {
+    ctx.command('test.rel', { permissions: ['custom.test.operator'] }).userFields(['locales']).action(async (argv) => {
       console.log(argv.session.text('general.name'), argv.session.user.locales)
       return await argv.session.execute('test.real')
     })
 
-    ctx.command('test.reload <plugin:string>').action(async (argv, name) => {
+    ctx.command('test.reload <plugin:string>', { permissions: ['custom.test.admin'] }).action(async (argv, name) => {
       const [key, parent] = this.findPlugin(name) ?? []
       if (!key) return 'Not found'
       ctx.loader.unloadPlugin(parent, key)
@@ -167,9 +215,34 @@ export class TestService extends Service {
       return 'Success ' + key
     })
 
+    ctx.command('test.cmd <cmd:text>')
+      .action(async ({ session }, cmd) => {
+        const argv = Argv.parse(cmd)
+        session.resolveCommand(argv)
+        const { command, options } = argv
+        if (!command) return 'Command not found'
+        const permissions = [`command.${command.name}`]
+        for (const option of Object.values(command._options)) {
+          if (option.name in options) {
+            permissions.push(`command.${command.name}.option.${option.name}`)
+          }
+        }
+        return (await ctx.permissions.test(permissions, session as any)) ? 'Success' : 'Fail'
+      })
+
+    ctx.command('testt.foo', { authority: null }).action(_ => 'a')
+    ctx.command('testt.bar')
+    console.log(ctx.$commander.get('testt').config)
+    console.log(ctx.$commander.get('testt.foo').config)
+    console.log(ctx.$commander.get('testt.bar').config)
+
     ctx.command('test.logger <label:string> <level:number>').action(async (argv, label, level) => {
       new Logger(label).level = level
     })
+
+    // ctx.on('internal/service', (name, oldValue) => {
+    //   console.log('internal/service', name, !oldValue ? 'enable' : 'disable')
+    // })
   }
 }
 
